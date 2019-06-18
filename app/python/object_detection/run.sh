1dcd43214d8919bde67bc2edfffca7c4d71c3d77
yoon <rky0930@gmail.com>
2019-06-17 21:52:38 -0700

    add run shell & pip requirements.txt

---
+++ app/python/object_detection/run.sh
@@ -0,0 +1,9 @@
+echo "----------------------TFLITE FLOAT-------------------------"
+python benchmark.py -c=configs/config_tf.yaml
+python accuracy_check.py -c=configs/config_tf.yaml -d=mscoco_val2017_inference_result.json
+echo "----------------------TFLITE FLOAT-------------------------"
+python benchmark.py -c=configs/config_tflite.yaml
+python accuracy_check.py -c=configs/config_tflite.yaml -d=mscoco_val2017_inference_result_tflite.json
+echo "---------------------TFLITE INT8------------------------"
+python benchmark.py -c=configs/config_tflite_quantized.yaml
+python accuracy_check.py -c=configs/config_tflite_quantized.yaml -d=mscoco_val2017_inference_result_tflite_quant.json
